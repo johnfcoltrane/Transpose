84076d3a81a66dfda9aa380acf39669bc0f054c8
johnfcoltrane <hy.p901is@gmail.com>
2018-03-17 18:53:30 +0900

    プロパティをクラスで処理

--- property.js
+++ property.js
@@ -1,7 +1,16 @@
 /***
  * フォントサイズ保存
  */
-var g_fontsize = 24;
+//var g_fontsize = 24;
+var g_properties = new Properties();
+
+/***
+ * データ保存用クラス
+ */
+function Properties() {
+	this.fontsize = 24;
+	this.text = '';
+}
 
 /***
  * フォントサイズを更新
@@ -9,8 +18,10 @@ var g_fontsize = 24;
 function fnUpdateFontSize(target, n){
 	//alert(target);
 	//alert(document.getElementById(target).style);
-	g_fontsize += n;
-	document.getElementById(target).style.fontSize = (g_fontsize)+'pt';
+	//g_fontsize += n;
+	g_properties.fontsize += n;
+	//document.getElementById(target).style.fontSize = (g_fontsize)+'pt';
+	document.getElementById(target).style.fontSize = (g_properties.fontsize)+'pt';
 	//alert(x);
 }
 
@@ -19,8 +30,10 @@ function fnUpdateFontSize(target, n){
  */
 function fnSave() {
 
-	window.localStorage.setItem('fontsize', g_fontsize);
-	alert("Saved:"+g_fontsize);
+	//window.localStorage.setItem('fontsize', g_fontsize);
+	window.localStorage.setItem('fontsize', g_properties.fontsize);
+	//alert("Saved:"+g_fontsize);
+	alert("Saved:"+g_properties.fontsize);
 
 }
 
@@ -28,7 +41,8 @@ function fnSave() {
  * ロード
  */
 function fnLoad() {
-	g_fontsize = parseInt(window.localStorage.getItem('fontsize'));
+	//g_fontsize = parseInt(window.localStorage.getItem('fontsize'));
+	g_properties.fontsize = parseInt(window.localStorage.getItem('fontsize'));
 	//alert("Loaded:"+g_fontsize);
 	fnUpdateFontSize('txt1', 0);
 }
