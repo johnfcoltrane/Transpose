c40372b219e83e24da35a6d71b1a7a5950712d50
johnfcoltrane <hy.p901is@gmail.com>
2018-03-17 19:50:49 +0900

    テキストの保存対応

--- property.js
+++ property.js
@@ -27,14 +27,27 @@ function fnUpdateFontSize(target, n){
 function fnSave() {
 
 	window.localStorage.setItem('fontsize', g_properties.fontsize);
-	alert("Saved:"+g_properties.fontsize);
+	
+	g_properties.textarea = document.getElementById('txt1').value;
+	window.localStorage.setItem('textarea', g_properties.textarea);
+	
+	alert("Saved:"+g_properties.textarea);
+	var t = window.localStorage.getItem('textarea');
+	alert('t='+t);
 
 }
-
+function fnTest1() {
+	//alert('Test1');
+	var t = window.localStorage.getItem('textarea');
+	alert('Test1 t='+t);
+}
 /***
  * ÉçÅ[Éh
  */
 function fnLoad() {
 	g_properties.fontsize = parseInt(window.localStorage.getItem('fontsize'));
+	g_properties.textarea = window.localStorage.getItem('textarea');
+	alert('text:'+g_properties.textarea);
 	fnUpdateFontSize('txt1', 0);
+	document.getElementById('txt1').value = g_properties.textarea;
 }
